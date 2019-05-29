b0aae8f335762272857687eaf633566fa07fcc98
Daniel Grießhaber <dangrie158@gmail.com>
2019-05-29 20:08:57 +0200

    reformatted README as reStructured Text

---
+++ README.rst
@@ -0,0 +1,15 @@
+PyTb (Python Toolbox)
+=====================
+
+This is a collection of useful snippets I regularly find myself to use during prototyping
+
+Installation
+------------
+
+via pip:
+
+    pip install py-toolbox
+
+via distutils:
+
+    python setup.py install
--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from distutils.core import setup
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
 
 setup(
     name='py-toolbox',
-    version='0.1.2',
+    version='0.1.4',
     author='Daniel Grießhaber',
     author_email='dangrie158@gmail.com',
     url='https://github.com/dangrie158/py-toolbox',
     packages=['pytb', 'pytb.test'],
+    include_package_data=True,
     license='MIT',
     description='A collection of commonly used python snippets',
-    long_description=open('README.md').read(),
+    long_description=read('README.rst'),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Topic :: Utilities',
