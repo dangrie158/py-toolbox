4748bf2555a15aa3bc9a6722dec7c03de29b783b
Daniel Grießhaber <dangrie158@gmail.com>
2019-06-11 17:03:28 +0200

    added initialization function of subsystems

--- pytb/__init__.py
+++ pytb/__init__.py
@@ -1,1 +1,34 @@
-__all__ = ["io", "importlib",]
+from .config import current_config
+from .importlib import NoModuleCacheContext, NotebookLoader
+from .rdb import install_hook as install_rdb
+
+__all__ = ["io", "importlib", "config", "rdb"]
+
+
+def init(verbose=True):
+    """
+    initialize the toolbox-subsystems using the current configuration
+
+    :param verbose: print what this function is setting up
+    """
+    config = current_config["init"]
+
+    output = print if verbose else lambda x: None
+
+    if config.getboolean('disable_module_cache'):
+        output("entering global pytb.NoModuleCacheContext")
+        NoModuleCacheContext().__enter__()
+    else:
+        output("'disable_module_cache' not set, skipping global context")
+
+    if config.getboolean("install_notebook_loader"):
+        output("installing NotebookLoader into 'sys.meta_path'")
+        NotebookLoader.install_hook()
+    else:
+        output("'install_notebook_loader' not set, skipping installation of hook")
+
+    if config.getboolean("install_rdb_hook"):
+        output("installing RDB as default debugger in 'sys.breakpointhook'")
+        install_rdb()
+    else:
+        output("'install_rdb_hook' not set, skipping installation of hook")
